9866cb269d19cea42ec22545d95f6077bd39beab
hyun <sarahs19991115@gmail.com>
2023-11-20 17:52:55 +0900

    [DOCS] 누락된 'README.md' 추가 [TEST] 누락된 'ApplicationTest.kt' 테스트 케이스 추가

--- src/test/kotlin/christmas/ApplicationTest.kt
+++ src/test/kotlin/christmas/ApplicationTest.kt
@@ -2,11 +2,38 @@ package christmas
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
+import christmas.ui.OutputView
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 
 class ApplicationTest : NsTest() {
+    private val outputView = OutputView()
+
+    @ValueSource(strings = ["a", " ", "123", "~!", "0"])
+    @ParameterizedTest
+    fun `날짜 예외 테스트`(input: String) {
+        assertSimpleTest {
+            runException(input)
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    @CsvSource(
+        value = ["3:제로콜라-a", "3:레드와아인-1", "3:제로콜라-0", "3:샴페인--4", "5:제로콜라1개", "2:4-타파스", "2:타파스- ", "30:타파스-30", "15:타파스-14,아이스크림-10"],
+        delimiter = ':'
+    )
+    @ParameterizedTest
+    fun `주문 예외 테스트`(date: String, order: String) {
+        assertSimpleTest {
+            runException(date, order)
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+        }
+    }
+
     @Test
     fun `모든 타이틀 출력`() {
         assertSimpleTest {
@@ -31,19 +58,20 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    //@CsvSource(value = ["5000:별", "12000, 트리", "42300, 산타"], delimiter = ':')
     @Test
-    fun `날짜 예외 테스트`() {
+    fun `배지 출력`() {
         assertSimpleTest {
-            runException("a")
-            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
+            outputView.printBadgeOfEvent(5000)
+            assertThat(output()).contains("별")
         }
-    }
-
-    @Test
-    fun `주문 예외 테스트`() {
         assertSimpleTest {
-            runException("3", "제로콜라-a")
-            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            outputView.printBadgeOfEvent(12000)
+            assertThat(output()).contains("트리")
+        }
+        assertSimpleTest {
+            outputView.printBadgeOfEvent(42300)
+            assertThat(output()).contains("산타")
         }
     }
 
